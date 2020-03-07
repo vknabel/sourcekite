837a80cb153c122415f96e8a89e4e044c99d6f57
Valentin Knabel <dev@vknabel.com>
2020-03-07 11:36:48 +0100

    Added executable product

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "sourcekite",
+    products: [
+        .executable(name: "sourcekite", targets: ["sourcekite"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/jpsim/SourceKitten", from: "0.27.0"),
     ],
